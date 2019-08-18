9a0063855c0c3bcfa50b67d6d06638c4a853e3e9
giuseppe <giusdr1993@gmail.com>
2019-08-18 23:53:30 +0200

    [Core] - Faster logging (but more .exe size!)

--- Source/Public/Core/Log/LogMacros.h
+++ Source/Public/Core/Log/LogMacros.h
@@ -3,8 +3,17 @@
 #include "Core/Log/Log.h"
 
 #if ZN_LOGGING
+#define FASTER_LOGGING 1
+#if FASTER_LOGGING 
 #define ZN_LOG(LogCategory, Verbosity, Format, ...)\
-Zn::Log::LogMsg(#LogCategory, Verbosity, Format, __VA_ARGS__);
+{\
+	static Name NAME_##LogCategory = Name(#LogCategory);\
+	Zn::Log::LogMsg(NAME_##LogCategory, Verbosity, Format, __VA_ARGS__);\
+}
+#else
+	#define ZN_LOG(LogCategory, Verbosity, Format, ...)\
+	Zn::Log::LogMsg(#LogCategory, Verbosity, Format, __VA_ARGS__);
+#endif
 #else
 #define ZN_LOG(...)
 #endif
