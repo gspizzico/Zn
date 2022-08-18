24287e17a14d62db610a1c701ec72710fde9a91b
dwagonlawd <giusdr1993@gmail.com>
2022-08-18 15:48:57 +0200

    Added tutorial shaders and compile script

---
+++ scripts/vk_compile_shaders.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import sys
+import shutil
+import filecmp
+
+from pathlib import Path
+
+SHADERS_RELATIVE_PATH = 'shaders'
+
+VULKAN_GLSLC = "Bin\glslc.exe"
+
+# Main Function
+try:
+    vulkan = os.getenv("VULKAN_SDK");
+
+    if(not os.path.exists(vulkan)):
+        raise Exception("Unable to find Vulkan directory")
+
+    compiler = os.path.join(vulkan, VULKAN_GLSLC)
+
+    if(not os.path.exists(compiler)):
+        raise Exception("Unable to find glslc compiler")
+
+    print("Using Vulkan GLSL compiler at: " + compiler)
+
+    root_dir = os.path.abspath(os.path.join(sys.argv[0], os.pardir, os.pardir))
+
+    shaders_dir = os.path.join(root_dir, SHADERS_RELATIVE_PATH);
+
+    if(not os.path.exists(shaders_dir)):
+        raise Exception("Unable to find shaders directory " + shaders_dir)
+    
+    for filename in os.scandir(shaders_dir):
+        if(filename.is_file() and not filename.name.endswith(".spv")):
+            name = filename.path
+            name_no_ext = Path(name).stem;
+            cmd = [compiler, name, "-o", os.path.join(shaders_dir, name_no_ext + ".spv")]
+            subprocess.run(cmd, shell=True)
+
+
+
+except Exception as error:
+    print('Caught error: ' + repr(error))
