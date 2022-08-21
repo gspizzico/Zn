30a8f7eb2d7ffda7c5a0dbd96c4c5a660f197f31
Giuseppe <giusdr1993@gmail.com>
2022-08-21 11:36:37 +0200

    build script update

--- scripts/PreLinking.bat
+++ scripts/PreLinking.bat
@@ -1,6 +1,7 @@
 @echo off
 
 if [%1]==[] goto :ErrorEmpty
+if [%2]==[] goto :ErrorEmpty
 
 if "%~1" =="Clean" goto :Clean
 
@@ -16,12 +17,12 @@ echo ====== Clean Completed ======
 
 :Build
 echo ====== Running SDL Build ======
-py %~dp0\sdl_build.py %~2
+py %~dp0sdl_build.py %~2 %~1
 echo ====== SDL Build Completed======
 goto :End
 	
 :ErrorEmpty
-echo Usage: "PreLink.bat [Clean | Build]"
+echo Usage: "PreLink.bat [Clean | Build] [Debug | Release | ReleaseWithTracy]"
 
 
 :End
