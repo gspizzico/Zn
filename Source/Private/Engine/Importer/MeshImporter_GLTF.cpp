314e30beb5e847b60d3e0fc464571f4adf239b54
dwagonlawd <giusdr1993@gmail.com>
2023-05-12 22:10:59 +0200

    Added tinygltf. Divided Mesh Importer into two files

---
+++ Source/Private/Engine/Importer/MeshImporter_GLTF.cpp
@@ -0,0 +1,8 @@
+#include <Znpch.h>
+#include <Engine/Importer/MeshImporter.h>
+#include <Rendering/RHI/RHIMesh.h>
+
+bool Zn::MeshImporter::Import_GLTF(const String& fileName, RHIMesh& mesh)
+{
+    return false;
+}
--- Source/Public/Engine/Importer/MeshImporter.h
+++ Source/Public/Engine/Importer/MeshImporter.h
@@ -2,6 +2,8 @@
 
 #include <Core/HAL/BasicTypes.h>
 
+DECLARE_LOG_CATEGORY(LogMeshImporter);
+
 namespace Zn
 {
 struct RHIMesh;
@@ -10,5 +12,9 @@ class MeshImporter
 {
   public:
     static bool Import(const String& fileName, RHIMesh& mesh);
+
+  private:
+    static bool Import_Obj(const String& fileName, RHIMesh& mesh);
+    static bool Import_GLTF(const String& fileName, RHIMesh& mesh);
 };
 } // namespace Zn
