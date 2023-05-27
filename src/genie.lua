3ef7151cdf8de3793f19c5e936afc115014c540d
dwagonlawd <giusdr1993@gmail.com>
2023-05-27 14:30:44 +0200

    moved core to its own file

--- src/genie.lua
+++ src/genie.lua
@@ -13,31 +13,28 @@ solution("ZnGenie")
     "Symbols"
   }
 
-  configuration "*-debug"
-  libdirs {
-    "../libs/Debug/**"
+  configurations {
+    "win-debug", 
+    "win-release",
   }
+
+  -- Defines
+
+  configuration "*-debug"
   defines {
     "_DEBUG",
     "ZN_DEBUG",
     "_CONSOLE"
-  }  
-  flags {
-    "FullSymbols"
-  }
+  } 
   
   configuration "*-release"
-  libdirs {
-    "../libs/Release/**"
-  }
   defines {
     "NDEBUG",
     "ZN_RELEASE",
     "_CONSOLE"
   }
-  flags {
-    "Optimize"
-  }
+
+  -- Platform Defines
 
   configuration "win-*"
   defines {
@@ -46,10 +43,30 @@ solution("ZnGenie")
     "_HAS_EXCEPTIONS=0",
   }
 
-  configurations {
-    "win-debug", 
-    "win-release",
+  -- Flags 
+  configuration "*-debug"
+  flags {
+    "FullSymbols"
   }
+  
+  configuration "*-release"
+  flags {
+    "Optimize"
+  }
+
+  -- Libs
+
+  configuration "*-debug"
+  libdirs {
+    "../libs/Debug/**"
+  }
+  
+  configuration "*-release"
+  libdirs {
+    "../libs/Release/**"
+  }
+
+  -- Suffixes
 
   configuration "*-debug"
     targetsuffix "-debug"
@@ -62,66 +79,4 @@ solution("ZnGenie")
   -- targetsubdir "./bin/"
   targetsubdir "./../Libs/Debug/"
 
--- project("math")
-  
---   kind "StaticLib"
-  
---   configuration "*"
-
---   files {
---     "./Math/**",
---     "./ThirdParty/glm/**.h",
---     "./ThirdParty/glm/**.cpp",
---     "./ThirdParty/glm/**.hpp",
---     "./ThirdParty/glm/**.inl",
---   }
-
---   includedirs {
---     "./Math/",
---     "./ThirdParty/glm/"
---   }
-
-project("core")
-
-  links {
-    "kernel32",
-    "user32",
-    "gdi32",
-    "rpcrt4",
-  }
-
-  solution "ZnGenie"
-  location = os.getcwd()
-
-  kind "StaticLib"
-
-  configuration "*"
-
-  pchheader "Core/Corepch.h"
-  pchsource "src/Core/Corepch.cpp"
-
-  files {
-    "./include/Core/**",
-    "./src/Core/**",
-    "./ThirdParty/wyhash/*",
-    "./ThirdParty/delegate/*",
-  }
-
-  includedirs {
-    "./include/",    
-    "./ThirdParty/",
-  }
-
-  configuration "win-*"
-
-  links {"mimalloc-static.lib"}
-  
-  files {
-    "./include/Core/Platforms/Windows/**",
-    "./src/Core/Platforms/Windows/**",
-    "./ThirdParty/mimalloc/**"
-  }
-  includedirs {
-    "./include/Core/Platforms/",
-    "./ThirdParty/mimalloc/"
-  }
+  dofile("project-core.lua")
---
+++ src/project-core.lua
@@ -0,0 +1,49 @@
+project("core")
+
+links {
+  "kernel32",
+  "user32",
+  "gdi32",
+  "rpcrt4",
+}
+
+-- solution "ZnGenie"
+-- location = os.getcwd()
+
+kind "StaticLib"
+
+pchheader "Core/Corepch.h"
+pchsource "src/Core/Corepch.cpp"
+
+-- Files
+configuration "*"
+files {
+  "./include/Core/**",
+  "./src/Core/**",
+  "./ThirdParty/wyhash/*",
+  "./ThirdParty/delegate/*",
+}
+
+configuration "win-*"
+files {
+  "./include/Core/Platforms/Windows/**",
+  "./src/Core/Platforms/Windows/**",
+  "./ThirdParty/mimalloc/**"
+}
+
+-- Includes
+configuration "*"
+includedirs {
+  "./include/",    
+  "./ThirdParty/",
+}
+
+configuration "win-*"  
+includedirs {
+  "./include/Core/Platforms/",
+  "./ThirdParty/mimalloc/"
+}
+
+-- Links
+configuration "win-*"  
+links {"mimalloc-static.lib"}
